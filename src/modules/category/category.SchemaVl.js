ec60bb896e657410af92c4556ea44923dab93c4c
MohamedAhmed893 <105381258+MohamedAhmed893@users.noreply.github.com>
2023-12-03 09:04:49 +0200

    update in validate

--- src/modules/category/category.SchemaVl.js
+++ src/modules/category/category.SchemaVl.js
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 export const createcategorySchema =Joi.object({
     name:Joi.string().min(3).max(20).required() ,
-    category:Joi.string().hex().length(24).required()
+   
 })
 export const updatecategorySchema =Joi.object({
     name:Joi.string().min(3).max(20).required() , 
